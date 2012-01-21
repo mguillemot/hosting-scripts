d59eab5b5517ced5e9da2341e3eef440df9e89a9
Matthieu Guillemot <mguillemot@npng.org>
2012-01-21 16:38:41 +0100

    Compute total size in backup view

--- backup/view.rb
+++ backup/view.rb
@@ -1,8 +1,9 @@
 ﻿#
 # View uploaded backups script.
 #
-# Author: Matthieu Guillemot 
+# Author: Matthieu Guillemot
 # Created: October 8, 2011
+# Updated: January 21, 2012 (compute total size)
 #
 
 # Note: a backup_config.rb file should be defined for this precise server purposes.
@@ -13,12 +14,18 @@ require 'date'
 require 'net/ftp'
 
 if USE_FTP_BACKUP
+  totalSize = 0
   puts "Backuped files are:"
   Net::FTP.open(FTP_SETTINGS[:host], FTP_SETTINGS[:user], FTP_SETTINGS[:password]) do |ftp|
-    ftp.nlst.each do |file| 
-      puts "  #{file}"
+    ftp.list.each do |listing|
+      parts = listing.split(/\s+/)
+      name = parts[-1]
+      size = parts[4].to_i
+      totalSize += size
+      puts "  #{size/1024/1024} Mb     #{name}"
     end
   end
+  puts "Total size was #{totalSize/1024/1024} Mb"
 else
   puts "ERROR: Not configured for using FTP backup."
 end
