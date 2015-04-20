32e8eab27e7aa23eba3db0deb02677f66b90a1a3
Erhune <erhune@littlebigmmo.com>
2015-04-20 12:12:07 +0900

    Fix "too many files" on tarsnap backup

--- backup/barman.sh
+++ backup/barman.sh
@@ -12,9 +12,9 @@ echo "$NOW | Pruning old snapshots..."
 
 NOW=$(date +%Y%m%d-%H%M%S)
 LAST=$(ls /var/lib/barman/$DB/base | tail -n 1)
-FILES=$(/usr/bin/barman list-files $DB $LAST)
+/usr/bin/barman list-files $DB $LAST > /tmp/tarsnap-files
 echo "$NOW | Sending new shapshot $LAST to tarsnap..."
-/usr/local/bin/tarsnap -c -f barman_$DB-$NOW $FILES
+/usr/local/bin/tarsnap -c -f barman_$DB-$NOW -T /tmp/tarsnap-files
 
 NOW=$(date +%Y%m%d-%H%M%S)
 echo "$NOW | Done!"
