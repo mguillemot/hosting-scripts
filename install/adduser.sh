e2cd6ecabb25af0e6087a0132473346c323fb67e
Erhune <erhune@littlebigmmo.com>
2017-01-13 14:50:55 +0900

    Fix user config

--- install/adduser.sh
+++ install/adduser.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Add a new user to the system.
+# Add a new user to the system, or fix its config.
 
 USER="$1"
 
@@ -16,8 +16,12 @@ if [ -z "$USER" ]; then
 fi
 
 echo
-echo "Creating user $USER..."
-adduser $USER
+if [ -d /home/$USER ]; then
+	echo "User already exists..."
+else
+	echo "Creating user $USER..."
+	adduser $USER
+fi
 
 echo
 echo "Setting default bash config..."
@@ -26,8 +30,12 @@ ln -s /etc/hosting-scripts/settings/bash_aliases /home/$USER/.bash_aliases
 chown $USER /home/$USER/.bash_aliases
 
 echo
-echo "Generating SSH key..."
-sudo -u $USER ssh-keygen
+if [ -f /home/$USER/.ssh/id_rsa ]; then
+	echo "SSH key already exists..."
+else
+	echo "Generating SSH key..."
+	sudo -u $USER ssh-keygen
+fi
 
 echo
 echo "Done!"
