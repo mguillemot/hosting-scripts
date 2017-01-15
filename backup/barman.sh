0d6c2afdb9c4c8c7bf6f1c1acc0518fbade47fec
Erhune <erhune@littlebigmmo.com>
2017-01-15 23:52:25 +0900

    Barman tarsnap support multiple dbs

--- backup/barman.sh
+++ backup/barman.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-DB="gosalpha"
+DBS=(gos)
+
+for DB in ${DBS[*]}; do
+	NOW=$(date +%Y%m%d-%H%M%S)
+	echo "$NOW | Starting barman snapshot of $DB..."
+	sudo -u barman /usr/bin/barman backup $DB
+
+	NOW=$(date +%Y%m%d-%H%M%S)
+	LAST=$(ls /var/lib/barman/$DB/base | tail -n 1)
+	/usr/bin/barman list-files $DB $LAST > /tmp/tarsnap-files
+	echo "$NOW | Sending new shapshot $LAST to tarsnap..."
+	/usr/local/bin/tarsnap -c -f barman_$DB-$NOW -T /tmp/tarsnap-files
+done
 
 NOW=$(date +%Y%m%d-%H%M%S)
-echo "$NOW | Starting barman snapshot of $DB..."
-sudo -u barman /usr/bin/barman backup $DB
-
-NOW=$(date +%Y%m%d-%H%M%S)
-LAST=$(ls /var/lib/barman/$DB/base | tail -n 1)
-/usr/bin/barman list-files $DB $LAST > /tmp/tarsnap-files
-echo "$NOW | Sending new shapshot $LAST to tarsnap..."
-/usr/local/bin/tarsnap -c -f barman_$DB-$NOW -T /tmp/tarsnap-files
-
-NOW=$(date +%Y%m%d-%H%M%S)
-echo "$NOW | Done!"
+echo "$NOW | All done!"
